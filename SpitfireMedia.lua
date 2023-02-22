d418f7d1a04faea9825c83ebd5b48b2a9622a5a7
Redstymp <14121390+Redstymp@users.noreply.github.com>
2023-02-22 16:38:08 +0100

    Add files via upload

---
+++ SpitfireMedia.lua
@@ -0,0 +1,45 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "spitfire", [[Interface\Addons\SharedMedia_SpitfireMedia\background\spitfire.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "blind_besoffen", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\blind_besoffen.mp3]]) 
+LSM:Register("sound", "spitfire_001_carry", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_001_carry.ogg]]) 
+LSM:Register("sound", "spitfire_002_blamage", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_002_blamage.mp3]]) 
+LSM:Register("sound", "spitfire_003_fuehrer", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_003_fuehrer.mp3]]) 
+LSM:Register("sound", "spitfire_004_toasty", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_004_toasty.mp3]]) 
+LSM:Register("sound", "spitfire_005_kenny_dead", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_005_kenny_dead.mp3]]) 
+LSM:Register("sound", "spitfire_006_ha_ha", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_006_ha_ha.mp3]]) 
+LSM:Register("sound", "spitfire_007_boss", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_007_boss.mp3]]) 
+LSM:Register("sound", "spitfire_008_diktator", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_008_diktator.mp3]]) 
+LSM:Register("sound", "spitfire_009_schlechte_spieler", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_009_schlechte_spieler.mp3]]) 
+LSM:Register("sound", "spitfire_010_pala_dd", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_010_pala_dd.mp3]]) 
+LSM:Register("sound", "spitfire_011_klopapier_druide", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_011_klopapier_druide.mp3]]) 
+LSM:Register("sound", "spitfire_012_dimi_dead", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_012_dimi_dead.mp3]]) 
+LSM:Register("sound", "spitfire_013_derp_yell", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_013_derp_yell.mp3]]) 
+LSM:Register("sound", "spitfire_014_no_risk_no_fun", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_014_no_risk_no_fun.mp3]]) 
+LSM:Register("sound", "spitfire_015_dr_zoidberg_woop", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_015_dr_zoidberg_woop.mp3]]) 
+LSM:Register("sound", "spitfire_016_sammeln", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_016_sammeln.mp3]]) 
+LSM:Register("sound", "spitfire_017_alarm", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_017_alarm.mp3]]) 
+LSM:Register("sound", "spitfire_018_buzzer", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_018_buzzer.mp3]]) 
+LSM:Register("sound", "spitfire_019_doh", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_019_doh.mp3]]) 
+LSM:Register("sound", "spitfire_020_beduerfnisse", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_020_beduerfnisse.mp3]]) 
+LSM:Register("sound", "spitfire_021_ja_ja", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_021_ja_ja.mp3]]) 
+LSM:Register("sound", "spitfire_s01_sailor_moon", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\spitfire_s01_sailor_moon.mp3]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
