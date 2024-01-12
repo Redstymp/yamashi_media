4b2e9c45f3edd74d6932311398ab80efdb780879
Redstymp <Redstymp@users.noreply.github.com>
2024-01-12 11:37:50 +0100

    finished renaming

---
+++ YamashiMedia.lua
@@ -0,0 +1,40 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+LSM:Register("background", "spitfire", [[Interface\Addons\SharedMedia_YamashiMedia\background\spitfire.tga]]) 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "", [[Interface\Addons\SharedMedia_YamashiMedia\border\.gitignore]]) 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "yamashi_001_carry", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_001_carry.ogg]]) 
+LSM:Register("sound", "yamashi_002_fuehrer", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_002_fuehrer.mp3]]) 
+LSM:Register("sound", "yamashi_003_derp_yell", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_003_derp_yell.mp3]]) 
+LSM:Register("sound", "yamashi_004_dr_zoidberg_woop", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_004_dr_zoidberg_woop.mp3]]) 
+LSM:Register("sound", "yamashi_005_alarm", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_005_alarm.mp3]]) 
+LSM:Register("sound", "yamashi_006_doh", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_006_doh.mp3]]) 
+LSM:Register("sound", "yamashi_007_nuke", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_007_nuke.mp3]]) 
+LSM:Register("sound", "yamashi_008_dead", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_008_dead.mp3]]) 
+LSM:Register("sound", "yamashi_009_quack", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_009_quack.mp3]]) 
+LSM:Register("sound", "yamashi_010_pinball", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_010_pinball.mp3]]) 
+LSM:Register("sound", "yamashi_011_valhalla", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_011_valhalla.mp3]]) 
+LSM:Register("sound", "yamashi_012_wooo", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_012_wooo.mp3]]) 
+LSM:Register("sound", "yamashi_s01_jaraxxus", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_s01_jaraxxus.mp3]]) 
+LSM:Register("sound", "yamashi_s02_jaws", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_s02_jaws.mp3]]) 
+LSM:Register("sound", "yamashi_s03_ice", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_s03_ice.mp3]]) 
+LSM:Register("sound", "yamashi_s04_intermission", [[Interface\Addons\SharedMedia_YamashiMedia\sound\yamashi_s04_intermission.mp3]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "", [[Interface\Addons\SharedMedia_YamashiMedia\statusbar\.gitignore]]) 
--- generateLua.bat
+++ generateLua.bat
@@ -1,62 +1,62 @@
 @echo off
-echo This script will now prepare the files for using SharedMedia_SpitfireMedia
+echo This script will now prepare the files for using SharedMedia_YamashiMedia
 
 echo Creating the TOC...
-echo ## Interface: 100002 > SharedMedia_SpitfireMedia.toc
-echo ## Title: SharedMedia_SpitfireMedia >> SharedMedia_SpitfireMedia.toc
-echo ## Dependencies: SharedMedia >> SharedMedia_SpitfireMedia.toc
-echo SpitfireMedia.lua >> SharedMedia_SpitfireMedia.toc
+echo ## Interface: 100002 > SharedMedia_YamashiMedia.toc
+echo ## Title: SharedMedia_YamashiMedia >> SharedMedia_YamashiMedia.toc
+echo ## Dependencies: SharedMedia >> SharedMedia_YamashiMedia.toc
+echo YamashiMedia.lua >> SharedMedia_YamashiMedia.toc
 echo Creating the file...
-echo local LSM = LibStub("LibSharedMedia-3.0") > SpitfireMedia.lua
+echo local LSM = LibStub("LibSharedMedia-3.0") > YamashiMedia.lua
 
 echo    BACKGROUND
-echo.>> SpitfireMedia.lua
-echo -- ----- >> SpitfireMedia.lua
-echo -- BACKGROUND >> SpitfireMedia.lua
-echo -- ----- >> SpitfireMedia.lua
+echo.>> YamashiMedia.lua
+echo -- ----- >> YamashiMedia.lua
+echo -- BACKGROUND >> YamashiMedia.lua
+echo -- ----- >> YamashiMedia.lua
 for %%F in (background\*.*) do (
 echo       %%~nF
-echo LSM:Register("background", "%%~nF", [[Interface\Addons\SharedMedia_SpitfireMedia\background\%%~nxF]]^) >> SpitfireMedia.lua
+echo LSM:Register("background", "%%~nF", [[Interface\Addons\SharedMedia_YamashiMedia\background\%%~nxF]]^) >> YamashiMedia.lua
 )
 
 echo    BORDER
-echo.>> SpitfireMedia.lua
-echo -- ----- >> SpitfireMedia.lua
-echo --  BORDER >> SpitfireMedia.lua
-echo -- ---- >> SpitfireMedia.lua
+echo.>> YamashiMedia.lua
+echo -- ----- >> YamashiMedia.lua
+echo --  BORDER >> YamashiMedia.lua
+echo -- ---- >> YamashiMedia.lua
 for %%F in (border\*.*) do (
 echo       %%~nF
-echo LSM:Register("border", "%%~nF", [[Interface\Addons\SharedMedia_SpitfireMedia\border\%%~nxF]]^) >> SpitfireMedia.lua
+echo LSM:Register("border", "%%~nF", [[Interface\Addons\SharedMedia_YamashiMedia\border\%%~nxF]]^) >> YamashiMedia.lua
 )
 
 echo    FONT
-echo.>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
-echo --   FONT>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
+echo.>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
+echo --   FONT>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
 for %%F in (font\*.ttf) do (
 echo       %%~nF
-echo LSM:Register("font", "%%~nF", [[Interface\Addons\SharedMedia_SpitfireMedia\font\%%~nxF]]^) >> SpitfireMedia.lua
+echo LSM:Register("font", "%%~nF", [[Interface\Addons\SharedMedia_YamashiMedia\font\%%~nxF]]^) >> YamashiMedia.lua
 )
 
 echo    SOUND
-echo.>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
-echo --   SOUND>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
+echo.>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
+echo --   SOUND>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
 for %%F in (sound\*.*) do (
 echo       %%~nF
-echo LSM:Register("sound", "%%~nF", [[Interface\Addons\SharedMedia_SpitfireMedia\sound\%%~nxF]]^) >> SpitfireMedia.lua
+echo LSM:Register("sound", "%%~nF", [[Interface\Addons\SharedMedia_YamashiMedia\sound\%%~nxF]]^) >> YamashiMedia.lua
 )
 
 echo    STATUSBAR
-echo.>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
-echo --   STATUSBAR>> SpitfireMedia.lua
-echo -- ----->> SpitfireMedia.lua
+echo.>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
+echo --   STATUSBAR>> YamashiMedia.lua
+echo -- ----->> YamashiMedia.lua
 for %%F in (statusbar\*.*) do (
 echo       %%~nF
-echo LSM:Register("statusbar", "%%~nF", [[Interface\Addons\SharedMedia_SpitfireMedia\statusbar\%%~nxF]]^) >> SpitfireMedia.lua
+echo LSM:Register("statusbar", "%%~nF", [[Interface\Addons\SharedMedia_YamashiMedia\statusbar\%%~nxF]]^) >> YamashiMedia.lua
 )
 
 :end_of_file
